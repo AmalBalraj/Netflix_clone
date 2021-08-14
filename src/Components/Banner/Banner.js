54ba085564d34339b8a14cd57bd98775bfef4423
Amal Balraj <amalbalraj99@gmail.com>
2021-08-14 16:54:59 +0530

    Movie rows API added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
+import {useEffect} from 'react'
+import axios from '../../axios'
+import {API_KEY,imageUrl} from '../../constants/constants'
 
 function Banner() {
-    return (
-        <div className = 'banner'>
+    const [movie, setMovie] = useState()
+    useEffect(() => {
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then(
+            (response)=>{
+                console.log(response.data.results[0])
+                setMovie(response.data.results[Math.floor(Math.random()*21)])
+            }
+        )
+    }, [])
+    return (  
+        <div
+        style = {{backgroundImage: `url(${movie? imageUrl+movie.backdrop_path: ""})`}} 
+        className = 'banner'>
             <div className = 'content'>
-                <h1 className = "title">Movie Name</h1>
+                <h1 className = "title">{movie ? movie.title : ""}</h1>
                 <div className = "banner_bottons">
                     <button className="botton">Play</button>
                     <button className="botton">My List</button>
                 </div>
-                <div className="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem optio praesentium numquam! Totam velit consequatur blanditiis deserunt eum et praesentium animi dolor, omnis perspiciatis quidem dignissimos, eligendi vitae pariatur temporibus!</div>
+                <div className="description">{movie ? movie.overview : ""}</div>
             </div>
             <div className="fade_bottom"></div>
         </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '7ee476a45b63c9041f5990cc263bf3f1';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
